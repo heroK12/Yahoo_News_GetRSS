8e09718ab99729be633e0bdf41004fe0980bda57
heroK12 <53800600+heroK12@users.noreply.github.com>
2020-01-07 20:12:50 +0900

    Add files via upload

---
+++ YahooNews_RSS/YahooNews_RSS/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace YahooNews_RSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.TopicSelect = new System.Windows.Forms.ComboBox();
+            this.NewsView = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(434, 12);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(152, 60);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "取得";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(592, 12);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(152, 60);
+            this.StopBtn.TabIndex = 1;
+            this.StopBtn.Text = "リセット";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TopicSelect
+            // 
+            this.TopicSelect.FormattingEnabled = true;
+            this.TopicSelect.Location = new System.Drawing.Point(86, 33);
+            this.TopicSelect.Name = "TopicSelect";
+            this.TopicSelect.Size = new System.Drawing.Size(121, 20);
+            this.TopicSelect.TabIndex = 3;
+            // 
+            // NewsView
+            // 
+            this.NewsView.Location = new System.Drawing.Point(12, 78);
+            this.NewsView.Name = "NewsView";
+            this.NewsView.Size = new System.Drawing.Size(776, 360);
+            this.NewsView.TabIndex = 4;
+            this.NewsView.Text = "";
+            this.NewsView.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.NewsView_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NewsView);
+            this.Controls.Add(this.TopicSelect);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.ComboBox TopicSelect;
+        private System.Windows.Forms.RichTextBox NewsView;
+    }
+}
+
---
+++ YahooNews_RSS/YahooNews_RSS/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace YahooNews_RSS
+{
+    public partial class Form1 : Form
+    {
+
+        string url;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TopicSelect.Items.Add("主要");         
+            TopicSelect.Items.Add("国内");         
+            TopicSelect.Items.Add("国際");         
+            TopicSelect.Items.Add("経済");         
+            TopicSelect.Items.Add("エンタメ");     
+            TopicSelect.Items.Add("スポーツ");     
+            TopicSelect.Items.Add("IT");           
+            TopicSelect.Items.Add("科学");         
+            TopicSelect.Items.Add("地域");         
+            TopicSelect.SelectedIndex = 0;
+            StartBtn.Enabled = true;
+            StopBtn.Enabled = false;
+            TopicSelect.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (TopicSelect.SelectedIndex)
+            {
+                case 0:
+                    url = "https://news.yahoo.co.jp/pickup/rss.xml";
+                    break;
+                case 1:
+                    url = "https://news.yahoo.co.jp/pickup/domestic/rss.xml";
+                    break;
+                case 2:
+                    url = "https://news.yahoo.co.jp/pickup/world/rss.xml";
+                    break;
+                case 3:
+                    url = "https://news.yahoo.co.jp/pickup/economy/rss.xml";
+                    break;
+                case 4:
+                    url = "https://news.yahoo.co.jp/pickup/entertainment/rss.xml";
+                    break;
+                case 5:
+                    url = "https://news.yahoo.co.jp/pickup/sports/rss.xml";
+                    break;
+                case 6:
+                    url = "https://news.yahoo.co.jp/pickup/computer/rss.xml";
+                    break;
+                case 7:
+                    url = "https://news.yahoo.co.jp/pickup/science/rss.xml";
+                    break;
+                case 8:
+                    url = "https://news.yahoo.co.jp/pickup/local/rss.xml";
+                    break;
+                default:
+                    NewsView.Text = "対応していない操作です";
+                    break;
+            }
+            try
+            {
+                StartBtn.Enabled = false;
+                StopBtn.Enabled = true;
+                TopicSelect.Enabled = false;
+
+                // RSS読み込み
+                XElement element = XElement.Load(url);
+
+                //NewsView.Text = element.Value;
+                
+                // channelの取得
+                XElement channelElement = element.Element("channel");
+
+                //title表示
+                XElement title = channelElement.Element("title");
+                NewsView.Text += title.Value;
+                NewsView.Text += "\r\n";
+
+
+
+                //itemの取得
+                IEnumerable<XElement> NewsItems = channelElement.Elements("item");
+
+                foreach (XElement NewsItem in NewsItems)
+                {
+                    XElement NewsTitle = NewsItem.Element("title");
+                    XElement Link = NewsItem.Element("link");
+                    NewsView.Text += NewsTitle.Value;
+                    NewsView.Text += "\r\n";
+                    NewsView.Text += Link.Value;
+                    NewsView.Text += "\r\n";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NewsView.Text = "";
+            StartBtn.Enabled = true;
+            StopBtn.Enabled = false;
+            TopicSelect.Enabled = true;
+        }
+
+        private void NewsView_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            Process.Start(e.LinkText);
+        }
+    }
+}
